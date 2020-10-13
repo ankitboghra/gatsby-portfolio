ea414db510f93c05fe1982df023f79a2598ff250
ankitboghra <ankitboghra@gmail.com>
2020-10-14 00:36:27 +0530

    variable name typo fix

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { ThemeContext } from 'providers/ThemeProvider';
 import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/skills.svg';
+import skills from 'assets/illustrations/skills.svg';
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
 
 export const Skills = () => {
@@ -12,7 +12,7 @@ export const Skills = () => {
     <Wrapper id="about">
       <SkillsWrapper as={Container}>
         <Thumbnail>
-          <img src={dev} alt="I’m Ankit and I’m a Frontend engineer and a freelancer!" />
+          <img src={skills} alt="I’m Ankit and I’m a Frontend engineer and a freelancer!" />
         </Thumbnail>
         <Details theme={theme}>
           <h1>More about me</h1>
